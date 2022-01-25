73ba221a537c253a275893983f08e99fbf72d82f
Diana <kozachenkodiana03@mail.ru>
2022-01-25 16:58:52 +0700

    Created form for new user

--- json-placeholder/src/app/components/post-page/post-page.component.ts
+++ json-placeholder/src/app/components/post-page/post-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-post-page',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-page.component.sass']
 })
 export class PostPageComponent implements OnInit {
+  public form: FormGroup = new FormGroup({
+    username: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', Validators.required),
+    website: new FormControl('', Validators.required)
+  })
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public submit(): void {
+    console.log(this.form)
   }
 
 }
